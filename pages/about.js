30b6c892a465aa2064c3d11995ed1b1085db126f
unknown <saffulkoranteng@gmail.com>
2022-08-21 10:26:42 -0700

    ADDED: About page

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import DashLayout from '../components/DashLayout';
+import Image from 'next/image';
+
+
+const Treatment =()=>{
+return (
+    <DashLayout>
+        <div class="max-w-4xl">
+            <h1 class="text-4xl font-bold">About Page</h1>
+        </div>
+    </DashLayout>
+);
+}
+
+export default Treatment;
